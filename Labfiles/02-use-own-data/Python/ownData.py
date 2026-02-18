018d2e489c40fefaab952fbcb78df47f7eccebd5
Aryan-Mangod <aryanmp2003@gmail.com>
2026-02-18 16:43:06 +0530

    Update ownData.py

--- Labfiles/02-use-own-data/Python/ownData.py
+++ Labfiles/02-use-own-data/Python/ownData.py
@@ -10,6 +10,9 @@ dotenv.load_dotenv()
 endpoint = os.environ.get("AZURE_OAI_ENDPOINT")
 api_key = os.environ.get("AZURE_OAI_KEY")
 deployment = os.environ.get("AZURE_OAI_DEPLOYMENT")
+azure_search_endpoint = os.environ.get("AZURE_SEARCH_ENDPOINT")
+azure_search_key = os.environ.get("AZURE_SEARCH_KEY")
+azure_search_index = os.environ.get("AZURE_SEARCH_INDEX")
 
 client = openai.AzureOpenAI(
     azure_endpoint=endpoint,
@@ -20,6 +23,15 @@ client = openai.AzureOpenAI(
 # Configure your data source
 
 
+# Get user input
+text = input('\nEnter a question:\n')
+
+# Send request to Azure OpenAI model
+completion = client.chat.completions.create(
+    model=deployment,
+    messages=[{"role": "user", "content": text}],
+    extra_body=extension_config
+)
 print(completion.choices[0].message.content)
 
 if showCitations:
